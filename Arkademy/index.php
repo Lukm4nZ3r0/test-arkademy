ba6df31f0ccd0e71a4dbd16933945082e6d3886a
Lukm4nZ3r0 <43030283+Lukm4nZ3r0@users.noreply.github.com>
2019-05-25 16:14:59 +0700

    Update index.php

--- Arkademy/index.php
+++ Arkademy/index.php
@@ -15,6 +15,7 @@ if(isset($_POST['tambah_programmer'])){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body class="container bg-primary mt-5">
+    <!-- Add Programmers name form -->
     <form action="" method="post" class="p-3 bg-light shadow rounded" style="width:80%;">
         <div class="form-group">
         <label for="nama">Tambah Baru:</label>
@@ -24,10 +25,14 @@ if(isset($_POST['tambah_programmer'])){
         </div>
         </div>
     </form>
-<?php 
+    <!-- End -->
+
+    <!-- Looping List Programmer -->
+    <?php 
     $query = mysqli_query($conn, "SELECT users.name, users.id FROM users");
     while($queryArray = mysqli_fetch_array($query)){
-?>
+    ?>
+
     <div class="row mt-3 bg-light rounded shadow p-3 ml-1" style="width:90%;">
         <div class="col">
         <h2><?= $queryArray['name'] ?></h2>
@@ -59,7 +64,10 @@ if(isset($_POST['tambah_programmer'])){
         </form>
         </div>
     </div>
-<?php } ?>
+
+    <?php } ?>
+    <!-- End Looping List Programmer -->
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
