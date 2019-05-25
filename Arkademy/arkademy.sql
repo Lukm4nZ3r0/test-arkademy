27646f9353c470b3a1685754db9bdc2b2400e782
Lukm4nZ3r0 <43030283+Lukm4nZ3r0@users.noreply.github.com>
2019-05-25 20:25:36 +0700

    Update arkademy.sql

--- Arkademy/arkademy.sql
+++ Arkademy/arkademy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2019 at 09:10 AM
+-- Generation Time: May 25, 2019 at 03:24 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -39,39 +39,11 @@ CREATE TABLE `skills` (
 --
 
 INSERT INTO `skills` (`id`, `name`, `user_id`) VALUES
-(1, 'HTML', 1),
-(2, 'CSS', 2),
-(3, 'PHP', 1),
-(4, 'Javascript', 1),
-(5, 'CSS5', 2),
-(6, 'CSS5', 2),
-(7, 'CSS3', 3),
-(8, 'CSS3', 3),
-(9, 'CSS2', 3),
-(10, 'Java', 4),
-(11, 'Kotlin', 1),
-(12, 'Javascript', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`) VALUES
-(1, 'Asep'),
-(2, 'Hakim'),
-(3, 'awdawd'),
-(4, 'Java');
+(13, 'PHP', 5),
+(14, 'Golang', 5),
+(15, 'Javascript', 5),
+(16, 'Ruby', 6),
+(17, 'Python', 6);
 
 --
 -- Indexes for dumped tables
@@ -84,12 +56,6 @@ ALTER TABLE `skills`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -97,13 +63,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `skills`
 --
 ALTER TABLE `skills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
